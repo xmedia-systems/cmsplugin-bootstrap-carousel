7d0b74c4311aaf1fa79b2835528dfaac60e5a529
xmedia-systems <alexeyb@gmx.de>
2012-09-26 15:07:37 +0200

    made image and caption optional + check domid is valid

--- cmsplugin_bootstrap_carousel/cms_plugins.py
+++ cmsplugin_bootstrap_carousel/cms_plugins.py
@@ -1,15 +1,28 @@
 # coding: utf-8
+import re
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cmsplugin_bootstrap_carousel.models import *
 from django.utils.translation import ugettext as _
 from django.contrib import admin
+from django.forms import ModelForm, ValidationError
+
+class CarouselForm(ModelForm):
+    class Meta:
+        model = Carousel
+    
+    def clean_domid(self):
+        data = self.cleaned_data['domid']
+        if not re.match(r'[a-zA-Z_]\w*', data):
+            raise ValidationError(_("The name must be a single word beginning with a letter"))
+        return data
 
 class CarouselItemInline(admin.TabularInline):
     model = CarouselItem
 
 class CarouselPlugin(CMSPluginBase):
     model = Carousel
+    form = CarouselForm
     name = _("Carousel")
     render_template = "cmsplugin_bootstrap_carousel/carousel.html"
 
